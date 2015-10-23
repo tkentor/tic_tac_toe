6c827416d89a4d6e5f2005f88ad5fbc7f4ca9cd3
Teddy Kentor <tkentor@gmail.com>
2015-10-23 10:51:36 -0400

    player checkboxes

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  alert("Welcome.")
+});
+
+$(document).ready(function(){
+  $(".row1").click(function(event){
+    $(".row1").css("background","blue")
+  });
+});
+
+$(document).ready(function(){
+  $(".row2").click(function(event){
+    $(".row2").css("background","blue")
+  });
+});
+
+$(document).ready(function(){
+  $(".row3").click(function(event){
+    $(".row3").css("background","blue")
+  });
+});
