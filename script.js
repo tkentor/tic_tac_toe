b2a15ef68e08ad380a0adc4be58722e8fb329466
Teddy Kentor <tkentor@gmail.com>
2015-10-23 13:45:08 -0400

    red and blue

--- script.js
+++ script.js
@@ -1,47 +1,44 @@
 $(document).ready(function(){
   alert("Welcome to the Tic-Tac-Thunderdome.")
 
-  if($("checkbox").checked){
-      $(".box1").click(function(event){
-        $(".box1").css("background","blue")
-      })
-  }else($(".box1").click(function(event){
-      $(".box1").css("background","red")
-  }));
-
-  $(".box2").click(function(event){
-    $(".box2").css("background","blue")
-  });
-  $(".box2").click(function(event){
-    $(".box2").css("background","red")
+  $("#box1").click(function(event){
+    $("#box1").toggleClass("player1")
   });
 
-  $(".box3").click(function(event){
-    $(".box3").css("background","blue")
+  $("#box1").click(function(event){
+    $(".player1").toggleClass("player2")
   });
 
-  $(".box4").click(function(event){
-    $(".box4").css("background","blue")
+  $("#box2").click(function(event){
+    $("#box2").css("background","blue")
   });
 
-  $(".box5").click(function(event){
-    $(".box5").css("background","blue")
+  $("#box3").click(function(event){
+    $("#box3").css("background","blue")
   });
 
-  $(".box6").click(function(event){
-      $(".box6").css("background","blue")
+  $("#box4").click(function(event){
+    $("#box4").css("background","blue")
   });
 
-  $(".box7").click(function(event){
-    $(".box7").css("background","blue")
+  $("#box5").click(function(event){
+    $("#box5").css("background","blue")
   });
 
-  $(".box8").click(function(event){
-    $(".box8").css("background","blue")
+  $("#box6").click(function(event){
+    $("#box6").css("background","blue")
   });
 
-  $(".box9").click(function(event){
-    $(".box9").css("background","blue")
+  $("#box7").click(function(event){
+    $("#box7").css("background","blue")
+  });
+
+  $("#box8").click(function(event){
+    $("#box8").css("background","blue")
+  });
+
+  $("#box9").click(function(event){
+    $("#box9").css("background","blue")
   });
 
 });
