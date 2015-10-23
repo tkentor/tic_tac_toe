c7e2f66107c0a4a6d805478bc4f057d9f251d3e2
Teddy Kentor <tkentor@gmail.com>
2015-10-23 10:16:46 -0400

    squares

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  alert("Welcome to the Tic-Tac-Thunderdome.")
+});
+
+$(document).ready(function(){
+  $(".row1").click(function(event){
+    $(".row1").css("background-color","yellow")
+  });
+});
